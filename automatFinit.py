bb725fd993636b130303e8d022bc1c1cd6eec5d9
pavelino-is-learning <pavel_emanuel_ioan@yahoo.com>
2021-12-04 14:46:49 +0200

    Laborator 4 Complet

---
+++ automatFinit.py
@@ -0,0 +1,113 @@
+import json
+
+class AutomatFinit():
+    def __init__(self, stari, alfabet, tranziti, stare_initiala, stari_finale ):
+        self.stari = stari
+        self.alfabet = alfabet
+        self.tranziti = {}
+        for elem in tranziti:
+            self.tranziti[(elem[0], elem[1])] = elem[2]
+        self.stare_initiala = stare_initiala
+        self.stari_finale = stari_finale
+
+    def print_automat(self):
+        print(f"Starile: {self.stari}")
+        print(f"Alfabetul de intrare: {self.alfabet}")
+        print("Functii de tranzitie: ")
+        for key, value in self.tranziti.items():
+            print(f"    {key} -> {value} ")
+        print()
+        print(f"Stare initiala: {self.stare_initiala}")
+        print(f"Stari finale: {self.stari_finale}")
+
+    def destinatie(self, stare, simbol):
+        return self.tranziti[(stare, simbol)] if (stare, simbol) in self.tranziti else None
+
+    def este_secventa_valida(self, stare, secventa, poz, drum_parcurs):
+        """
+         Verifica in mod recursiv daca secventa este valida
+        :param stare: Starea in care ne aflam
+        :param secventa: Secventa pe care o validam
+        :param poz: Pozitia simbolului curent din secventa
+        :param drum_parcurs, o lista care va tine minte toate starile prin care am trecut
+        :return: 1 Daca secventa este acceptata , 0 daca nu este acceptata
+        """
+        # Obtinem toate destinatile posibile din stare cu sinbolul cu pozitia poz din secenta data
+        destinatii = self.destinatie(stare, secventa[poz])
+        # print(destinatii)
+        if destinatii is None:  # daca nu gasim nici o destinatie e clar ca nu mai putem continua pe drumul acesta
+            return 0
+        # Conditia de iesire: practim verificam daca la untima pozitie avem in destinatii un element care se afla in
+        #       starile finale. Daca da returnam 1, daca nu inseamna ca drumul ales e gresit si ne intoarcem
+        if poz == len(secventa)-1:
+            for elem in destinatii:
+                if elem in self.stari_finale:
+                    return 1
+            return 0
+        # Partea recusiva: aici cautam un drum astfel incat sa validam secventa
+        # O sa luat toate starile din destinatii si verificam daca pe una din ele pot ajunge cumva la o stare final
+        # cu un ultimul simbol din secventa
+        for elem in destinatii:
+            print(drum_parcurs + elem)
+            aux = self.este_secventa_valida(elem, secventa, poz + 1, drum_parcurs + elem)
+            if aux == 1:
+
+                return 1
+        return 0
+
+    def verificare_secventa(self, secventa):
+        """
+        Wraper pt functia este_secventa
+        :param secventa: O adunatura de simboluri
+        :return:
+        """
+        return "Valid" if self.este_secventa_valida(self.stare_initiala, secventa, 0, self.stare_initiala) else "Invalid"
+
+    def tranziti_in_productii(self):
+        productii = {}
+        for tranzitie, stari in self.tranziti.items():
+            print(tranzitie, stari)
+            if tranzitie[0] not in productii:
+                productii[tranzitie[0]] = []
+
+            for elem in stari:
+                if elem not in self.stari_finale:
+                    productie = tranzitie[1] + elem
+                else:
+                    productie = tranzitie[1]
+                my_productie = productii[tranzitie[0]]
+                my_productie.append(productie)
+                productii[tranzitie[0]] = my_productie
+        return productii
+
+
+    def conversie_af_gr(self):
+        """
+        Transforma automatul finit intr-o gramatica regulara
+        :return:
+        """
+        gramatica = {"neterminale": [elem for elem in self.stari if elem not in self.stari_finale],
+                     "terminale": self.alfabet, "initial": self.stare_initiala,
+                     "productii": self.tranziti_in_productii()}
+        with open("af_in_gr.json", "w") as file:
+            json.dump(gramatica, file)
+
+
+def main():
+    with open("automat_finit.json") as f:
+        data = dict(json.load(f))
+
+    at = AutomatFinit(data['stari'], data['alfabet'], data['tranziti'],
+                      data['stare_initiala'], data['stari_finale'])
+
+    at.print_automat()
+    simbol = "c"
+    stare = "A"
+    # aici primim un simbol si o stare . Cautam destinatia
+    print(f"Tranzitile din starea {stare} cu simbolul {simbol} sunt {at.destinatie(stare, simbol)}")
+    secventa = input("Dati secventa: ")
+    print(at.verificare_secventa(secventa))
+
+
+if __name__ == '__main__':
+    main()
---
+++ main.py
@@ -0,0 +1,118 @@
+import json
+from automatFinit import AutomatFinit
+
+class Gramatica:
+    def __init__(self, neterminale, terminale, productii, initial):
+        self.neterminale = list(neterminale)
+        self.terminale = terminale
+        self.productii = productii
+        self.initial = initial
+
+    @staticmethod
+    def pretty_productie_form(productie):
+        pretty_productie = ""
+        pretty_productie += f"{productie[0]} ->"
+        for elem in productie[1]:
+            pretty_productie += f" {elem} |"
+        return pretty_productie
+
+    def __str__(self):
+        print_productii = ""
+        for productie in self.productii:
+            print_productii += "\t" + self.pretty_productie_form(productie) + "\n"
+        return f"Simboluri neterminale: {self.neterminale} \n" \
+               f"Simboluri terminale {self.terminale} \n" \
+               f"Productii \n{print_productii}" \
+               f"Simbol initial {self.initial}"
+
+    def verificare_sintaxa(self, drumuri):
+        for drum in drumuri:
+            if 0 < len(drum) < 3:
+                if len(drum) == 1:
+                    if drum not in self.terminale:
+                        return False
+                elif drum[0] not in self.terminale or drum[1] not in self.neterminale:
+                    return False
+            else:
+                return False
+        return True
+
+    def verificare_regularitate(self):
+        """
+        Verifica regularitatea automatului
+        :return: True daca este Gramatica regulara, False altfel
+        """
+        for productie in self.productii:
+            if (len(productie[0]) != 1 and productie[0] not in self.neterminale) or\
+                    not self.verificare_sintaxa(productie[1]):
+                print(f"Nu este Gramatica regulara")
+                return False
+            return True
+
+    def producti_in_tranziti(self):
+        """
+        Transforma productile gramatici regulare in tranziti pentru automatul finit
+        """
+        tranziti = {}
+        for productie in self.productii:
+
+            for elem in productie[1]:
+                tranzitie = None
+                if len(elem) == 2:
+                    if (productie[0],elem[0]) not in tranziti:
+                        tranziti[(productie[0],elem[0])] = [elem[1]]
+                    else:
+                        my_tranzitie = tranziti[(productie[0],elem[0])]
+                        my_tranzitie.append(elem[1])
+                        tranziti[(productie[0],elem[0])] = my_tranzitie
+                else:
+                    if (productie[0], elem[0]) not in tranziti:
+                        tranziti[(productie[0], elem[0])] = ["K"]
+                    else:
+                        my_tranzitie = tranziti[(productie[0],elem[0])]
+                        my_tranzitie.append("K")
+                        tranziti[(productie[0], elem[0])] = my_tranzitie
+        my_tranziti = []
+        for key, value in tranziti.items():
+            my_tranziti.append([key[0], key[1], value])
+        return my_tranziti
+
+    def conversie_gr_af(self):
+        """
+        Transforma o gramatica regulara intr-un automat finit :D
+        """
+        # verifica daca gramatica este regulara
+        if self.verificare_regularitate():
+            my_list = self.neterminale
+            my_list.append("K")
+            automat_finit = {"stari_finale": ["K"], "stari": my_list,
+                             "stare_initiala": self.initial, "alfabet": self.terminale,
+                             "tranziti": self.producti_in_tranziti()}
+            with open("gr_in_af.json", "w") as file:
+                json.dump(automat_finit, file)
+        else:
+            print("Nu se poate face conversia pentru ca Gramatica NU ESTE regulara")
+
+
+def main():
+    with open("gramatica.json") as f:
+        data = dict(json.load(f))
+
+    gramatica = Gramatica(data["neterminale"], data["terminale"], data["productii"], data["initial"])
+    print(gramatica)
+    gramatica.conversie_gr_af()
+
+
+    with open("gr_in_af.json") as f:
+        data = dict(json.load(f))
+
+    at = AutomatFinit(data['stari'], data['alfabet'], data['tranziti'],
+                      data['stare_initiala'], data['stari_finale'])
+
+    at.print_automat()
+    at.conversie_af_gr()
+
+
+
+if __name__ == '__main__':
+    main()
